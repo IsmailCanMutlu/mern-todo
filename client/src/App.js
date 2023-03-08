ea024df1a1c3f30cd2446f5d41da1cfeed9a0234
IsmailCanMutlu <109580162+IsmailCanMutlu@users.noreply.github.com>
2023-03-08 09:41:42 +0300

    frontend

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
+import Navbaar from './component/Navbaar';
+
+function App() {
+  return (
+    <Navbaar/>
+  );
+}
+
+export default App;
