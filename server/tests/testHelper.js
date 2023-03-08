08e82ddeec3361bb470478c5d27d8757188a32fd
IsmailCanMutlu <109580162+IsmailCanMutlu@users.noreply.github.com>
2023-03-08 09:23:35 +0300

    Create testHelper.js

---
+++ server/tests/testHelper.js
@@ -0,0 +1,18 @@
+const initialUsers = [
+    {
+        description: "first user",
+        likes: 1
+    },
+    {
+        description: "second user",
+        likes: 2
+    },
+    {
+        description: "third user",
+        likes: 3
+    }
+]
+
+export {
+    initialUsers
+}
