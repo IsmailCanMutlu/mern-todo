70e0c3f7963a6a25e3b0ccc6a18d9cd0d9274497
IsmailCanMutlu <109580162+IsmailCanMutlu@users.noreply.github.com>
2023-03-20 09:52:18 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
-import Navbaar from './component/Navbaar';
+import Navbaar from './components/Navbaar';
+import Home from './components/Home';
+import Register from './components/Register';
+
+
+
+
 
 function App() {
   return (
-    <Navbaar/>
+   <>
+    <Navbaar />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/edit/:id" component={Edit} />
+      <Route exact path="/view/:id" component={Details} />
+    </Switch>
+   
+   </>
   );
 }
 
