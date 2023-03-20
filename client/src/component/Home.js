3aab465aecf5800f1fa6a60d3ee6b626e8d29fa4
IsmailCanMutlu <109580162+IsmailCanMutlu@users.noreply.github.com>
2023-03-20 09:53:22 +0300

    Create Home.js

---
+++ client/src/component/Home.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect, useContext } from 'react'
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+import CreateIcon from '@mui/icons-material/Create';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import { NavLink } from 'react-router-dom';
+import { adddata, deldata } from './context/ContextProvider';
+import { updatedata } from './context/ContextProvider'
+
+const Home = () => {
+
+    const [getuserdata, setUserdata] = useState([]);
+    console.log(getuserdata);
+
+    const { udata, setUdata } = useContext(adddata);
+
+    const {updata, setUPdata} = useContext(updatedata);
+
+    const {dltdata, setDLTdata} = useContext(deldata);
+
+    const getdata = async () => {
+
+        const res = await fetch("/register", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        const data = await res.json();
+        console.log(data);
+
+        if (res.status === 422 || !data) {
+            console.log("error ");
+
+        } else {
+            setUserdata(data)
+            console.log("get data");
+
+        }
+    }
+
+    useEffect(() => {
+        getdata();
+    }, [])
+
+}
